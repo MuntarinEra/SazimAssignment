3f1ab1eeb0dfe62422b02342c52a04e8f4d77f00
MuntarinEra <muntarinjannat@gmail.com>
2022-07-24 23:21:44 +0600

    git commit -m

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+//components
+
+
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Fragment>
+    <div className="container">
+      
+      <Login />
+      
+    </div>
+  </Fragment>
+  );
+}
+
+export default App;
